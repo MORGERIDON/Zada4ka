584dba2ca3cb1ef0ace5ed514d3604e1e275f65c
Morgeridon <Morgeridon@yandex.ru>
2022-04-14 17:31:44 +0500

    text

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-
+// tekst
         int[] randomInts = getRandomInts(8);
         printArray(randomInts);
 
